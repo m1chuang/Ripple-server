be07c533fdf32c4ce1c1905b47886c613979d25a
michael chuang <mchuang927@gmail.com>
2015-01-25 16:34:40 -0800

    delete unsuesd

--- bitapi/route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var bitapi = express.Router();
-
-bitapi.route('/game/mine')
-bitapi.route('/game/mine/cashout')
-bitapi.route('/game/mine/checkboard')
-
-bitapi.route('/refresh_balance')
-bitapi.route('/withdraw')
