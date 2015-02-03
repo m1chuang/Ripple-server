4e06295fbfc89dce05c93566b7a6d67036099665
michael chuang <mchuang927@gmail.com>
2015-02-03 12:23:27 -0800

    Add riot example as starter for client app UI

--- client/app/app.js
+++ client/app/app.js
@@ -4,6 +4,11 @@ var async = require('async');
 var array = require('array');
 var PUBNUB = require('pubnub');
 
+
+/********************
+**  PUBNUB CLIENT  **
+*********************/
+
 var PubnubClient=function(){
   this.initStatus=false;
   this.pubnub;
@@ -20,90 +25,113 @@ PubnubClient.prototype.init = function(params){
   console.log('PUBNUB initializingi.'+params);
 };
 
-PubnubClient.prototype.sub = function(channel){
+PubnubClient.prototype.sub = function(channel, cb){
     this.pubnub.subscribe({
       channel: channel,
      // windowing: 1000,
-      presence: function(m){console.log(m)},
-      callback: function(message){
-        console.log(message);
-        switch(message.type){
-          case server:
-            break;
-          case message:
-            break;
-          case update:
-            break;
-        }
-      },
-      connect: function(){console.log("Connected to "+channel)},
-      disconnect: function(){console.log("Disconnected")},
-      reconnect: function(){console.log("Reconnected")},
-      error: function(){console.log("Network Error")}
+      presence: (m)=>{console.log(m)},
+      callback:cb,
+      connect: ()=>{console.log("Connected to "+channel)},
+      disconnect: ()=>{console.log("Disconnected")},
+      reconnect: ()=>{console.log("Reconnected")},
+      error: ()=>{console.log("Network Error")}
     });
   };
 
-PubnubClient.prototype.pub = function(channel){
+
+PubnubClient.prototype.pub = function(channel,msg){
     this.pubnub.publish({
          channel : channel,
-         message :'msg'
+         message :msg
      });
-    console.log('Publish to channel '+channel);
+    console.log('Publishing to channel '+channel);
   };
 
+
+/*****************
+**  API CLIENT  **
+ ****************/
+
 var apiClient = function (device_info,env){
-  this.device_info=device_info;
+  this.data=device_info;
   this.env=env;
   this.pn = new PubnubClient();
 
-  this.post = function(endpoint, params, next){
-    var url = this.device_info.baseURL[env||'prod']+endpoint;
-    params.auth_token = this.device_info.auth_token|| params.auth_token;
+  this.post = (endpoint, params, next)=>{
+    var url = this.data.baseURL[env||'prod']+endpoint;
+    params.auth_token = this.data.auth_token|| params.auth_token;
     console.log('Sending POST to...'+url+ '\n'+params);
-    unirest.post(url).header('Accept','application/json').send(params).end(function(response){
+    unirest.post(url).header('Accept','application/json').send(params).end((response)=>{
       console.log(response.body);
       next(null,response.body);
     });
   };
 
-  this.postMulti = function(endpoint, data, next){
-    var url = this.device_info.baseURL[env||'prod']+endpoint;
+  this.postMulti = (endpoint, data, next)=>{
+    var url = this.data.baseURL[env||'prod']+endpoint;
     console.log('Sending POST Multi to...'+url+ '\n'+data);
-    unirest.post(url).field('lat',this.device_info.location.lat).field('lon',this.device_info.location.lon).field('auth_token',this.device_info.auth_token).attach('file',data.image).end(function(response){
+    unirest.post(url).field('lat',this.data.location.lat).field('lon',this.data.location.lon).field('auth_token',this.data.auth_token).attach('file',data.image).end((response)=>{
       console.log(response.body);
       next(null,response.body)
     });
   };
 
-  this.put = function(endpoint, data, next){
-    var url = this.device_info.baseURL[env||'prod']+endpoint;
-    data.auth_token = this.device_info.auth_token|| data.auth_token;
-    unirest.put(url).header('Accept','application/json').send(data).end(function(response){
-      console.log('api put...'+ '\n'+response.body);
+  this.put = (endpoint, data, next)=>{
+    var url = this.data.baseURL[env||'prod']+endpoint;
+    data.auth_token = this.data.auth_token|| data.auth_token;
+    console.log('api put...');
+    unirest.put(url).header('Accept','application/json').send(data).end((response)=>{
+      console.log(response.body);
       next(null,response.body);
     })
   }
 }
 apiClient.prototype.initPubnub = function(){
   this.pn.init({
-     publish_key: this.device_info.pubnub_pub_key,
-     subscribe_key: this.device_info.pubnub_sub_key,
-     auth_key:this.device_info.pubnub_auth_key
+     publish_key: this.data.pubnub_pub_key,
+     subscribe_key: this.data.pubnub_sub_key,
+     auth_key:this.data.pubnub_auth_key
   });
   console.log('PUBNUB initialized.');
 };
+
 apiClient.prototype.connectServer = function(){
-  this.pn.sub(this.device_info.channel_uuid)
+  this.pn.sub(this.data.channel_uuid,(msg)=>{
+        console.log(msg);
+        switch(msg.type){
+          case '001'://new logins from subscribtion
+            console.log(this.data.subscribe_list.items.find({uuid:msg.uuid}).moments);
+            console.log(this.data.subscribe_list.items.find({uuid:msg.uuid}));
+            this.data.subscribe_list.items.find({uuid:msg.uuid}).moments.push({
+              image_url:msg.image_url,
+              status:msg.status,
+              //timestamp:Date.now()
+            });
+
+            break;
+          case '002':
+            break;
+          case '003':
+            break;
+        };
+  });
   console.log('Connecting server...');
 };
+apiClient.prototype.backup = function(){
+  console.log('Soving some device info to server...');
+};
 apiClient.prototype.pokeServer = function(){
-  this.pn.pub(this.device_info.channel_uuid);
+  this.pn.pub(this.data.channel_uuid);
   console.log('Poking server...');
 };
-function LoginUI (apiClient,data){
+
+/***************
+**  LOGIN UI  **
+ ***************/
+
+function LoginUI (apiClient){
 
   this.api = apiClient;
-  this.data = data;
   this.photo = '';
   this.status = '';
 
@@ -111,25 +139,24 @@ function LoginUI (apiClient,data){
     console.log('init_moment...');
     this.photo=photo;
     this.api.postMulti('/moment',{image:photo||'./img.jpg'},
-      (function(err, response){
+      (err, response)=>{
         console.log(response);
         console.log('Update device info.');
-        this.api.device_info.auth_token=response.new_auth_token;
-        this.api.device_info.uuid = response.uuid;
+        this.api.data.auth_token=response.new_auth_token;
+        this.api.data.uuid = response.uuid;
         if(next){
           next();
         }else{
           console.log('Subscribing to server...');
-          this.api.pn.sub(response.uuid);
+          this.api.connectServer();
         }
-      }).bind(this)
-    );
+      });
   };
 
   this.submit_moment = function(status,next){
 
     this.status=status || 'yo';
-    this.api.put('/moment', {status:status},(function(err, response){
+    this.api.put('/moment', {status:status},(err, response)=>{
       console.log('submit_moment...');
       if(!next){
         console.log(response);
@@ -138,19 +165,22 @@ function LoginUI (apiClient,data){
         if(next)next('err');
       }else{
 
-        this.data.explore_list.rePopulate(response.explore_list,function(){
+        this.api.data.explore_list.rePopulate(response.explore_list,()=>{
           console.log('populating explore...');
           if(next)next();
         });
       }
 
-    }).bind(this));
+    });
   };
 
 };
 
-var moment_list = function(api){
-  this.api = api;
+/****************************
+**  moment list data type  **
+ ****************************/
+
+function moment_list(){
   this.items = array();
 }
 
@@ -158,58 +188,72 @@ moment_list.prototype.rePopulate = function(info_list,next){
   console.log('populate list itmes...');
   //console.log(info_list);
   this.items = array();
-  async.eachSeries(info_list,(function(i,next){
-    this.items.push({image_url:i.image_url,distance:i.distance,status:i.status,action_token:i.action_token
+  async.eachSeries(info_list,(i,next)=>{
+    this.items.push({
+      uuid:i.uuid,
+      image_url:i.image_url,
+      distance:i.distance,
+      status:i.status,
+      action_token:i.action_token
     });
     next();
-  }).bind(this),
-  (function(err){
-    next();
-  }).bind(this));
+  },
+  (err)=>{next();}
+  );
 }
 
 
 
-function ExploreUI (apiClient, data){
-  this.data = data;
+/*****************
+**  EXPLORE UI  **
+ *****************/
+
+function ExploreUI (apiClient){
   this.api=apiClient;
-  this.data.explore_list.subscribe = function(index, next){
+  this.api.data.explore_list.subscribe = (index, next)=>{
     console.log('substribing to explore item: '+index);
-    var item = this.items[index];
-    this.api.post('/moment/action', {action_token:item.action_token.subscribe},(function(response){
-      console.log(response);
-      this.data.subscribe_list.add(item);
-    }).bind(this));
+    var item = this.api.data.explore_list.items[index];
+    this.api.post('/moment/action', {action_token:item.action_token.subscribe},(response)=>{
+      this.api.data.subscribe_list.addItem(item);
+    });
   }
   this.nextPage = function(){
     console.log('[POST api/explore/nextPage]');
     return this
   }
   this.refresh = function(next){
-    this.api.post('/moment/explore', {auth_token:''},(function(err,response){
-        this.data.explore_list.rePopulate(response.explore_list,(function(){
+    this.api.post('/moment/explore', {auth_token:''},(err,response)=>
+      {
+        this.api.data.explore_list.rePopulate(response.explore_list,()=>
+        {
           console.log('Refresh explore list...');
-          console.log(this.data)
+          console.log(this.api.data)
           next();
-        }).bind(this));
-      }).bind(this)
-    );
-  }
-};
+        });
+      });
+  };
+};//eof
 
-function SubscribeUI(apiClient, data){
-  this.data = data;
+
+/*******************
+**  SUBSCRIBE UI  **
+********************/
+
+function SubscribeUI(apiClient){
   this.api=apiClient;
-  this.data.subscribe_list.add = function(item){
+  this.api.data.subscribe_list.addItem = (item)=>{
     console.log('Adding '+item.status+ ' to subscribe list...');
-    this.data.subscribe_list.push({
+    this.api.data.subscribe_list.items.push({
+      uuid:item.uuid,
       image_url:item.image_url,
-      distance:itme.distance,
+      distance:item.distance,
       status:item.status,
       nickname:'',
-      moments:[]
+      moments:new array()
     })
   };
+
+
   this.nextPage = function(data){
     console.log('api call to subscriber nextPage');
     return this
@@ -218,10 +262,16 @@ function SubscribeUI(apiClient, data){
     console.log('api call to subscriber refresh');
     return tihs
   }
-};
+
+};//eof
+
+
+/***************
+**  MAIN APP  **
+****************/
 
 function App(params,next){
-  this.device_info =
+  this.data =
   {
     location:{lat:20,lon:11},
     auth_token:'',
@@ -233,77 +283,75 @@ function App(params,next){
       local:'http://127.0.0.1:8000/api',
       prod:'http://glimpse-prod-env-a22zhqiyzs.elasticbeanstalk.com/api'
     },
-    login_status:true
-  }
-  this.api = new apiClient(this.device_info, params.env || 'prod');
-  this.data =
-  {
-    explore_list:new moment_list(this.api),
-    subscribe_list:new moment_list(this.api),
+    login_status:true,
+
+    subscribe_list:new moment_list(),
+    explore_list:new moment_list(),
     chat_list:[],
     self_list:[]
   }
-  this.content = {
-    login:new LoginUI(this.api, this.data),
-    explore:new ExploreUI(this.api, this.data),
-    subscriber:new SubscribeUI(this.api, this.data),
+  this.api = new apiClient(this.data, params.env || 'prod');
+
+  this.control = {
+    login:new LoginUI(this.api),
+    explore:new ExploreUI(this.api),
+    subscriber:new SubscribeUI(this.api),
   }
   this.init= function(next){
 
     this.api.post('/device',{
       auth_token:'new'
-    },(function(err,params){
-        this.device_info.auth_token = params.auth_token;
-        this.device_info.pubnub_auth_key = params.pubnub_key;
-        this.device_info.channel_uuid = params.uuid;
-        this.data.subscribe_list = params.friend_list;
+    },(err,params)=>
+      {
+        this.data.auth_token = params.auth_token;
+        this.data.pubnub_auth_key = params.pubnub_key;
+        this.data.channel_uuid = params.uuid;
         this.data.login_status = params.relogin;
         this.api.initPubnub();
-        next.call(this);
-      }).bind(this)
+
+        if(next)next();
+      }
     );
   };
   this.init(next);
 }
 
 App.prototype.move = function(){
-  this.device_info.location.lat += 1;
-  this.device_info.location.lon += 1;
+  this.data.location.lat += 1;
+  this.data.location.lon += 1;
 };
 App.prototype.login = function(status, image, location){
   this.data.location = location || this.data.location;
-  this.content.login.init_moment(image,(function(){
-    setTimeout((function(){
-      this.content.login.submit_moment(status,
-      (function(){
-        console.log(this);
-      }).bind(this));
-    }).bind(this),1000);
-  }).bind(this));
+  this.control.login.init_moment(image,()=>
+  {
+    setTimeout(()=>
+    {
+      this.control.login.submit_moment(status,()=>console.log(this));
+    },1000);
+  });
 };
-App.prototype.view_explore = function(){
-  this.content.explore.refresh((function(){
-    console.log(this.data.explore_list.items);
-  }).bind(this));
-};
-App.prototype.subscribe = function(index){
-  this.data.explore_list.subscribe(index,function(item){
+App.prototype.v_exp = function(){
+  this.control.explore.refresh(()=>
+    console.log(this.data.explore_list.items)
+  );
 
+};
+App.prototype.sub = function(index){
+  this.data.explore_list.subscribe(index,(item)=>{
     console.log(response);
   })
 };
-App.prototype.view_subscriber = function(){
-  this.content.subscriber.refresh();
+App.prototype.v_sub = function(){
+  this.control.subscriber.refresh();
+};
+App.prototype.v_info = function(){
+  this.api.post('/device/info',{},(device)=>{
+    console.log(device);
+  });
 };
 
-
-var a= new App({env:'local'},function(){
-  /*
-  setTimeout((function(){
-        this.login('big mac','./img.jpg');
-    }).bind(this),1000);
-*/
-  //this.view_explore();
-
+var a;
+a= new App({env:'local'},()=>{
+  console.log(a);
 });
-//a.content.login.submit_moment('mc_status');
+//a.login('mc','./img.jpg')
